1d592b8e29f4a281220ea4c363c053bd6f863150
sharib45 <sharibshahid45@gmail.com>
2024-04-28 15:33:54 +0530

    ss

--- 2 Variables and DataTypes/variables.c
+++ 2 Variables and DataTypes/variables.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
+#define  age 18;
 
 int main(){
 
+
+const float PI = 3.14;
 
 int a = 5;
 float out;
 
 printf("%d" ,a) ;
 
+out = 12.5;
 
+
+
+float area = a*out;
+
+printf("%d",area);
 
 }
