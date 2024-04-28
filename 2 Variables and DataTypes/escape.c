616651e4bb8ef74f79ed3f10cf916a7d13a56799
sharib45 <sharibshahid45@gmail.com>
2024-04-28 16:53:36 +0530

    ss

---
+++ 2 Variables and DataTypes/escape.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+
+printf("sharib\nHusain\n");
+
+printf("sharib\thusain\n");
+
+printf("sharib\bhusain\n");
+
+printf("sharib \"husain\"\n");
+
+printf("sharib \'husain\'\n");
+
+printf("sharib\\husain");
+
+}
