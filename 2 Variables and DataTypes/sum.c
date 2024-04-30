e7489229ed891038381280aaa21307519e326f91
sharib45 <sharibshahid45@gmail.com>
2024-04-30 16:54:06 +0530

    ss

---
+++ 2 Variables and DataTypes/sum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main () {
+
+int num1 ,num2, sum;
+
+
+printf("please enter number one:");
+
+scanf("%d",&num1);
+
+printf("please enter number two :");
+
+scanf("%d",&num2);
+
+sum = num1+num2;
+
+printf("Sum of A and B is: %d", sum); 
+  
+  
+  return 0; 
+
+
+
+
+
+}
