55844a770d843a85b94e3d90afd644ca71873ca2
sharib45 <sharibshahid45@gmail.com>
2024-05-28 00:07:47 +0530

    dd

---
+++ 4DecisionControl/if.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main(){
+
+int age;
+
+printf("Please enter your age :");
+
+scanf("%d",&age);
+
+
+if(age >=18){
+ 
+ printf("you are eligible for licances ");
+
+
+}
+
+
+printf(" your age is %d",age);
+return 0;
+
+}
