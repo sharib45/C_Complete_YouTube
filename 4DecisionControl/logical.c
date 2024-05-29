6b4364f26dc86888a479656393b96cf947a30e73
sharib45 <sharibshahid45@gmail.com>
2024-05-29 15:14:45 +0530

    sss

---
+++ 4DecisionControl/logical.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+
+int age ;
+
+char is_indian;
+
+
+printf("welcome in arto bijnor \n please enter your age :");
+
+scanf("%d",&age);
+
+printf("are you indian Citizen please enter [y\n]");
+
+scanf(" %c",&is_indian);
+
+
+if (is_indian == 'Y' || (age  >=18 && age <70 )){
+
+printf("you are eligible for liciense");
+
+
+
+}
+
+else{
+
+
+printf("you are not eligible ");
+
+}
+
+
+
+//if(is_indian == 'y'){
+
+
+// if(age <18)
+// {
+
+// printf("you are to young to apply \n");
+
+
+// } 
+
+// else if(age >70){
+
+
+// printf("you are too old to apply\n");
+
+
+
+// }
+
+// else{
+
+// printf("you are eligible for liciense");
+
+
+
+// }
+
+// }
+
+
+
+// else{
+
+// printf("only indian citizen can apply\n");
+
+
+// }
+
+printf(" your age is %d",age);
+
+
+
+
+}
