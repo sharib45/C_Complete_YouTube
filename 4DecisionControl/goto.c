1331d25150b54f569b38c28ce3b3924c9b83aa40
sharib45 <sharibshahid45@gmail.com>
2024-05-30 13:52:58 +0530

    xxx

---
+++ 4DecisionControl/goto.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+int main(){
+
+int num;
+
+input_number:
+
+printf("please enter the number");
+
+scanf("%d",&num);
+
+
+if(num !=10){
+
+
+goto input_number;
+
+
+}
+
+return 0;
+
+
+
+
+
+
+}
