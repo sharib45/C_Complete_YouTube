140d73051c8b61202793110d214fded1962970ae
sharib45 <sharibshahid45@gmail.com>
2024-06-04 07:48:06 +0530

    ff

---
+++ 5IterationandLoop/while.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+int num=1;
+
+
+while(num<=10)
+{
+
+
+printf("num is %d \n",num);
+
+num = num+1;
+}
+
+printf("loop has ended\n");
+
+
+
+
+int num2=100;
+
+
+while(num2 >0){
+
+
+printf("num is %d \n",num2);
+
+num2--;
+
+
+  
+}
+
+
+
+
+
+return 0;
+
+
+
+}
+
+
